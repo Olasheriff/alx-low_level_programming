3875ec3fd797543c655f3270c8f44e12a464755e
Olasheriff <olasheriff.99@gmail.com>
2023-08-24 04:45:42 -0700

    100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -4,6 +4,7 @@
 /**
  * rot13 - encoder rot13
  * @s: pointer to string params
+ *
  * Return: *s
  */
 
@@ -11,13 +12,12 @@ char *rot13(char *s)
 {
 	int i;
 	int j;
-	char data1[] =
-"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	char datarot[] =
-"NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; j < S2; j++)
+		for (j = 0; j < 52; j++)
 		{
 			if (s[i] == data1[j])
 			{
