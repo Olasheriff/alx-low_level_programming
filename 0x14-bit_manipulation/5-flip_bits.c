3c1869a11a12621a61ca4ff8d1a1d2c0a4e5b3c7
Olasheriff <olasheriff.99@gmail.com>
2023-09-28 04:34:49 -0700

    0x14-bit_manipulation

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -8,11 +8,11 @@
  *
  * Return: number of bits to change
  */
-unsigned int flip_bits(unsigned long int n, unsigned long int m);
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	int i, count = 0;
 	unsigned long int current;
-	unsined long int exclusive = n ^ m;
+	unsigned long int exclusive = n ^ m;
 
 	for (i = 63; i >= 0; i--)
 	{
